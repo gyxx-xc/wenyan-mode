e1316ff244089578ced24283a09b74c22bce9d72
gyxx-xc <xdll_xc@outlook.com>
2024-04-01 13:45:59 +0800

    copy the wenyan from other repo

---
+++ wenyan.el
@@ -0,0 +1,146 @@
+;;; wenyan.el --- Wenyan major mode
+
+;; Copyright (C) 2001  Free Software Foundation, Inc.
+
+;; Author: StefanMonnier
+;; Keywords: extensions
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Wenyan major mode
+
+;;; Code:
+
+(defvar wenyan-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\C-j" 'newline-and-indent)
+    map)
+  "Keymap for `wenyan-mode'.")
+
+(defvar wenyan-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?\u3001 ".") ;; 、
+    (modify-syntax-entry ?\u3002 ".") ;; 。
+    (modify-syntax-entry ?\u300C "w") ;; 「
+    (modify-syntax-entry ?\u300D "w") ;; 」
+    (modify-syntax-entry ?\u300E "\"") ;; 『
+    (modify-syntax-entry ?\u300F "\"") ;; 』
+    st)
+  "Syntax table for `wenyan-mode'.")
+
+;; type word 元|物|爻|術|言|列|數
+;; key word 乃行是術曰|若其不然者|乃止是遍|乃歸空無|欲行是術|若其然者|其物如是|乃得矣|恆為是|之術也|必先得|是術曰|之物也|云云|其餘|中之|為是|之長|乃止|若非|或若|乃得|是謂|者|若|遍|之|充|銜|凡|也
+;; operator 中有陽乎|所餘幾何|中無陰乎|不等於|不大於|不小於|等於|大於|小於|加|乘|除|變|以|於|減
+;; built in 不知何禍歟|不復存矣|如事不諧|姑妄行此|吾嘗觀|之禍歟|乃作罷|名之曰|書之|以施|之禍|嗚呼|之義|昔之|方悟|是矣|今有|吾有|之書|物之|夫|中|今|取|噫|曰|施|豈|有|蓋謂|或云
+;; number 負|·|又|零|〇|一|二|三|四|五|六|七|八|九|十|百|千|萬|億|兆|京|垓|秭|穰|溝|澗|正|載|極|分|釐|毫|絲|忽|微|纖|沙|塵|埃|渺|漠
+;; const 陰|陽|其
+;; string 『*』|「「*」」
+;; comment 批曰|注曰|疏曰
+;; punctuation 。|、
+;; variable & function 「*」
+(defvar wenyan-font-lock-keywords
+  `(
+    (,(rx
+       (and (or "疏曰" "注曰") (?? "。") (*? blank)
+            (or (and "「「" (*? anything) "」」")
+                (and "『" (*? anything) "』"))))
+     . 'font-lock-comment-face)
+
+    (,(rx
+       (and "批曰" (?? "。") (*? blank)
+            (or (and "「「" (*? anything) "」」")
+                (and "『" (*? anything) "』"))))
+     . 'font-lock-doc-face)
+
+    (,(rx
+       (or (and "「「" (*? anything) "」」")
+           (and "『" (*? anything) "』")))
+     . 'font-lock-string-face)
+
+    (,(rx
+       (or "名之曰" "中之")
+       (submatch "「" (*? anything) "」"))
+     1 'font-lock-variable-name-face)
+
+    (,(rx "施"
+          (submatch "「" (*? anything) "」"))
+     1 'font-lock-function-use-face
+     )
+
+    (,(rx
+       (and "「" (*? anything) "」"))
+     . 'font-lock-variable-use-face)
+
+    (,(regexp-opt '("不知何禍歟" "不復存矣" "如事不諧" "姑妄行此" "吾嘗觀" "之禍歟" "乃作罷" "名之曰" "書之" "以施" "之禍" "嗚呼" "之義" "昔之" "方悟" "是矣" "今有" "吾有" "之書" "物之" "夫" "中" "今" "取" "噫" "曰" "施" "豈" "有" "蓋謂" "或云" "乃行是術曰" "若其不然者" "乃止是遍" "乃歸空無" "欲行是術" "若其然者" "其物如是" "乃得矣" "恆為是" "之術也" "必先得" "是術曰" "之物也" "云云" "其餘" "中之" "為是" "之長" "乃止" "若非" "或若" "乃得" "是謂" "者" "若" "遍" "之" "充" "銜" "凡" "也") t)
+     . 'font-lock-keyword-face)
+
+    (,(regexp-opt '("中有陽乎" "所餘幾何" "中無陰乎" "不等於" "不大於" "不小於" "等於" "大於" "小於" "加" "乘" "除" "變" "以" "於" "減") t)
+     . 'font-lock-operator-face)
+
+    (,(rx (or "陰" "陽" "其"))
+     . 'font-lock-constant-face)
+
+    (,(regexp-opt '("元" "物" "爻" "術" "言" "列" "數") t)
+     . 'font-lock-type-face)
+
+    (,(regexp-opt '("負" "·" "又" "零" "〇" "一" "二" "三" "四" "五" "六" "七" "八" "九" "十" "百" "千" "萬" "億" "兆" "京" "垓" "秭" "穰" "溝" "澗" "正" "載" "極" "分" "釐" "毫" "絲" "忽" "微" "纖" "沙" "塵" "埃" "渺" "漠") t)
+     . 'font-lock-number-face)
+
+    (,(regexp-opt '("。" "、") t)
+     . 'font-lock-punctuation-face)
+    )
+  "Keyword highlighting specification for `wenyan-mode'.")
+
+;; (defvar wenyan-imenu-generic-expression
+;;   ...)
+
+;; (defvar wenyan-outline-regexp
+;;   ...)
+
+;;;###autoload
+(define-derived-mode wenyan-mode prog-mode "Wenyan"
+  "A major mode for editing Wenyan files."
+  :syntax-table wenyan-mode-syntax-table
+  (setq-local comment-start (regexp-opt '("批曰" "注曰" "疏曰") t))
+  (setq-local comment-end "」」")
+  (setq-local font-lock-defaults
+              '(wenyan-font-lock-keywords))
+  (setq-local indent-line-function 'wenyan-indent-line)
+  ;; (setq-local imenu-generic-expression
+  ;;             wenyan-imenu-generic-expression)
+  ;; (setq-local outline-regexp wenyan-outline-regexp)
+  )
+
+;;; Indentation
+
+(defun wenyan-indent-line ()
+  "Indent current line of Wenyan code."
+  (interactive)
+  (let ((savep (> (current-column) (current-indentation)))
+        (indent (condition-case nil (max (wenyan-calculate-indentation) 0)
+                  (error 0))))
+    (if savep
+        (save-excursion (indent-line-to indent))
+      (indent-line-to indent))))
+
+(defun wenyan-calculate-indentation ()
+  "Return the column to which the current line should be indented."
+  0)
+
+(provide 'wenyan)
+;;; wenyan.el ends here
